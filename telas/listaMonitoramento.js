275383adfa1250cce1272cb782333977e6b52a56
Davi Monteiro <davimonteirope@gmail.com>
2025-07-03 18:43:36 -0300

    feat: telas complementares

--- telas/listaMonitoramento.js
+++ telas/listaMonitoramento.js
@@ -1,22 +1,5 @@
 let monitoramentos = [
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '20/11/2023', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '15/04/2020', dataFim: '23/01/2023' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '03/02/2024', dataFim: '15/07/2024' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '18/03/2022', dataFim: '25/09/2023' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '12/02/2025', dataFim: '' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '03/06/2025', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '01/08/2021', dataFim: '15/08/2021' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '30/11/2024', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '22/05/2023', dataFim: '19/01/2024' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '15/04/2022', dataFim: '10/01/2023' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '05/04/2025', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '14/07/2020', dataFim: '08/11/2022' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '05/02/2024', dataFim: '20/05/2024' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '02/12/2024', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '11/09/2022', dataFim: '04/04/2023' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '07/01/2024', dataFim: '18/06/2024' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '19/03/2025', dataFim: '' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '01/02/2025', dataFim: '' }];
+    { id: 1, status: 'Em Andamento', descricao: 'Descrição relacionada ao Monitoramento', dataInicio: '20/11/2023', dataFim: '' }];
 
 let monitoramentoSelecionado = null;
 
@@ -26,28 +9,40 @@ function renderizarTabela() {
     corpoTabela.innerHTML = '';
 
     if (monitoramentos.length === 0) {
-        corpoTabela.innerHTML = '<tr><td colspan="5" class="text-center">Nenhum cliente encontrado.</td></tr>';
+        corpoTabela.innerHTML = '<tr><td colspan="5" class="text-center">Nenhum Monitoramento encontrado.</td></tr>';
         return;
     }
 
-    monitoramentos.forEach(monitoramentos => {
+    monitoramentos.forEach(monitoramento => {
         const tr = document.createElement('tr');
         tr.innerHTML = `
-                    <td>${monitoramentos.status}</td>
-                    <td>${monitoramentos.descricao}</td>
-                    <td>${monitoramentos.dataInicio}</td>
-                    <td>${monitoramentos.dataFim}</td>
+                    <td>${monitoramento.status}</td>
+                    <td>${monitoramento.descricao}</td>
+                    <td>${monitoramento.dataInicio}</td>
+                    <td>${monitoramento.dataFim}</td>
                     <td class="text-center">
-                        <a href="#" class="btn btn-primary btn-sm" title="Editar" onclick="editarMonitoramento(${monitoramentos.id})">
+                        <a href="editarMonitoramento.html?id=${monitoramento.id}" class="btn btn-primary btn-sm" title="Editar">
                             <i class="fa fa-pencil"></i>
                         </a>
-                        <button class="btn btn-danger btn-sm ms-2" title="Deletar" onclick="preparaDelecao(${monitoramentos.id})">
+                        <button class="btn btn-danger btn-sm ms-2" title="Deletar" onclick="preparaDelecao(${monitoramento.id})">
                             <i class="fa fa-trash"></i>
                         </button>
                     </td>
                 `;
         corpoTabela.appendChild(tr);
     });
+}
+
+function preparaDelecao(monitoramentoId) {
+    const confirmou = window.confirm("Tem certeza que deseja excluir este Monitoramento?");
+    if (confirmou) {
+        const indexParaDeletar = monitoramentos.findIndex(v => v.id === monitoramentoId);
+        if (indexParaDeletar > -1) {
+            monitoramentos.splice(indexParaDeletar, 1);
+        }
+        renderizarTabela();
+        alert("Monitoramento excluído com sucesso!");
+    }
 }
 
 
--- telas/listaVistoria.js
+++ telas/listaVistoria.js
@@ -1,22 +1,5 @@
 let vistorias = [
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '20/11/2023', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '15/04/2020', dataFim: '23/01/2023' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '03/02/2024', dataFim: '15/07/2024' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '18/03/2022', dataFim: '25/09/2023' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '12/02/2025', dataFim: '' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '03/06/2025', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '01/08/2021', dataFim: '15/08/2021' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '30/11/2024', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '22/05/2023', dataFim: '19/01/2024' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '15/04/2022', dataFim: '10/01/2023' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '05/04/2025', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '14/07/2020', dataFim: '08/11/2022' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '05/02/2024', dataFim: '20/05/2024' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '02/12/2024', dataFim: '' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '11/09/2022', dataFim: '04/04/2023' },
-    { status: 'Finalizado', descricao: 'Descrição relacionada a vistoria', dataInicio: '07/01/2024', dataFim: '18/06/2024' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '19/03/2025', dataFim: '' },
-    { status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '01/02/2025', dataFim: '' }];
+    { id: 1, status: 'Em Andamento', descricao: 'Descrição relacionada a vistoria', dataInicio: '20/11/2023', dataFim: '' }];
 
 let vistoriaSelecionado = null;
 
@@ -26,28 +9,41 @@ function renderizarTabela() {
     corpoTabela.innerHTML = '';
 
     if (vistorias.length === 0) {
-        corpoTabela.innerHTML = '<tr><td colspan="5" class="text-center">Nenhum cliente encontrado.</td></tr>';
+        corpoTabela.innerHTML = '<tr><td colspan="5" class="text-center">Nenhuma Vistoria encontrada.</td></tr>';
         return;
     }
 
-    vistorias.forEach(vistorias => {
+    vistorias.forEach(vistoria => {
         const tr = document.createElement('tr');
         tr.innerHTML = `
-                    <td>${vistorias.status}</td>
-                    <td>${vistorias.descricao}</td>
-                    <td>${vistorias.dataInicio}</td>
-                    <td>${vistorias.dataFim}</td>
+                    <td>${vistoria.status}</td>
+                    <td>${vistoria.descricao}</td>
+                    <td>${vistoria.dataInicio}</td>
+                    <td>${vistoria.dataFim}</td>
                     <td class="text-center">
-                        <a href="#" class="btn btn-primary btn-sm" title="Editar" onclick="editarMonitoramento(${vistorias.id})">
+                        <a href="editarVistoria.html?id=${vistoria.id}" class="btn btn-primary btn-sm" title="Editar">
                             <i class="fa fa-pencil"></i>
                         </a>
-                        <button class="btn btn-danger btn-sm ms-2" title="Deletar" onclick="preparaDelecao(${vistorias.id})">
+                        <button class="btn btn-danger btn-sm ms-2" title="Deletar" onclick="preparaDelecao(${vistoria.id})">
                             <i class="fa fa-trash"></i>
                         </button>
                     </td>
                 `;
         corpoTabela.appendChild(tr);
     });
+}
+
+
+function preparaDelecao(vistoriaId) {
+    const confirmou = window.confirm("Tem certeza que deseja excluir esta vistoria?");
+    if (confirmou) {
+        const indexParaDeletar = vistorias.findIndex(v => v.id === vistoriaId);
+        if (indexParaDeletar > -1) {
+            vistorias.splice(indexParaDeletar, 1);
+        }
+        renderizarTabela();
+        alert("Vistoria excluída com sucesso!");
+    }
 }
 
 
